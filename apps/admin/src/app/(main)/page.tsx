3bca56596a99dfb776e5ed02426295510c774b35
alexandre-notor <amanichambu48@gmail.com>
2023-08-14 11:36:46 +0200

    fix: auto load

--- apps/admin/src/app/(main)/page.tsx
+++ apps/admin/src/app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const router = useRouter();
     useEffect(() => {
         router.replace("/dash");
-    });
+    }, [router]);
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <SpinLoader />
